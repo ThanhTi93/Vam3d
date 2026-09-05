5eca8bf90cc91fb333f7605683e8fcfb61014877
thanhti <codebydevproduction@gmail.com>
2026-09-05 23:10:52 +0700

    fix: change analytics scripts strategy to lazyOnload to prevent hydration race condition

--- app/layout.tsx
+++ app/layout.tsx
@@ -156,9 +156,9 @@ export default function RootLayout({
         </AuthProvider>
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-FND8B4GNWE"
-          strategy="afterInteractive"
+          strategy="lazyOnload"
         />
-        <Script id="google-analytics" strategy="afterInteractive">
+        <Script id="google-analytics" strategy="lazyOnload">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
